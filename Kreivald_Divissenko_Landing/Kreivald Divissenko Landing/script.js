d25792c2bfdb7c429b382604f5d192e4e75c81af
pupil <pupil@ivkhk.ee>
2022-04-29 11:23:43 +0300

    Update

--- Kreivald_Divissenko_Landing/Kreivald Divissenko Landing/script.js
+++ Kreivald_Divissenko_Landing/Kreivald Divissenko Landing/script.js
@@ -1,8 +1,13 @@
+setInterval(remove, 10);
+
 function remove(){
-    if(window.innerWidth<428) {
-    document.getElementById("symbol").src = "";
+    if(window.innerWidth <= 550) {
+        document.getElementById("pilot").src = "Images/Plane in clouds.png";
+        document.getElementById("pilot").style.width = String(window.innerWidth)+"px";
+        
     }
     else{
-    document.getElementById("symbol").src = "";
+        document.getElementById("pilot").src = "Images/asideright.png";
+        document.getElementById("pilot").style.width = "17.03125%";
     }
 }
