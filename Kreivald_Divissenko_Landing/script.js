d9f29f9e9272ac8a92d0ba205196112a7785f326
pupil <pupil@ivkhk.ee>
2022-05-18 09:11:08 +0300

    Update

--- Kreivald_Divissenko_Landing/script.js
+++ Kreivald_Divissenko_Landing/script.js
@@ -1,13 +1,33 @@
+let size2 = 0;
+
 setInterval(remove, 10);
 
 function remove(){
-    if(window.innerWidth*0.6184971098265895953757225433526 <= 428) {
-        document.getElementById("pilot").src = "Images/Plane in clouds.png";
-        document.getElementById("pilot").style.width = String(window.innerWidth)+"px";
-        
+    var size = window.innerWidth;
+    
+    var a =`<div>
+    <h1>Туры в Турцию по <br>лучшим ценам</h1>
+    <p>Скидки до 20% в разные места Турции. Купите тур для 3-х или более людей - <br>получите 5% бонус к скидке.</p>
+    <button>Подробнее</button>
+    </div>`
+
+    var b = `<div>
+    <h1>Туры в Турцию по <br>лучшим ценам</h1>
+    <p>Скидки до 20% в разные места Турции. Купите тур для 3-х или более людей - <br>получите 5% бонус к скидке.</p>
+    <button>Подробнее</button>
+    </div><img id="pilot" class="pilot" src="../Images/asideright.png" alt="">`
+
+    if (size2 < size || size2 > size) {
+        if(window.innerWidth*0.6184971098265895953757225433526 <= 428) {
+            document.getElementById("main").innerHTML = a;
+            с = true;
+        }
+        if(window.innerWidth*0.6184971098265895953757225433526 > 428){
+            document.getElementById("main").innerHTML = b;
+            c = false;
+        }
+        size2 = size;
     }
-    else{
-        document.getElementById("pilot").src = "Images/asideright.png";
-        document.getElementById("pilot").style.width = "17.03125%";
-    }
+
+
 }
